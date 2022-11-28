e6a2771366109ba1837822de834faabd181e4406
Killian <cluzel.killian@gmail.com>
2022-11-28 18:42:47 +0100

    add loading + Readme

--- src/Components/chapter.js
+++ src/Components/chapter.js
@@ -2,8 +2,8 @@ import React from "react";
 
 function Chapter({ oneChapter }) {
     return (
-        <div class={'flex w-full'}>
-            <img class='w-16' src={oneChapter?.url} />
+        <div className={oneChapter?.valid ? 'flex w-full' : 'flex w-full  bg-gray-500 opacity-70'}>
+            <img className='w-16' src={oneChapter?.url} />
             <div>{oneChapter?.title}</div>
             {console.log(oneChapter)}
         </div>
